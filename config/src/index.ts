b161f25df60515f4e8ecd0e9328f442daa15a716
kasefuchs <kasefuchs@protonmail.com>
2025-11-04 22:08:34 +0300

    chore: add deploy workflow

--- config/src/index.ts
+++ config/src/index.ts
@@ -1,23 +1,22 @@
 import { domainTree, recordToDomainModifier, RecordType } from "@domains/zones";
 
-const REG_NONE = NewRegistrar("none");
-const DNS_BIND = NewDnsProvider("bind");
+const dns = NewDnsProvider("dns");
+const registrar = NewRegistrar("none");
 
 Object.keys(domainTree).forEach((domain) => {
+  D(domain, registrar, DnsProvider(dns));
+
   const mapping = domainTree[domain]!;
-
-  D(domain, REG_NONE, DnsProvider(DNS_BIND));
-
   Object.keys(mapping).forEach((name) => {
     const zone = mapping[name]!;
 
     Object.values(RecordType).forEach((recordType) => {
       const records = zone.records[recordType];
-      if (!records || !records.length) return;
-
-      const modifiers = records.map((record) => recordToDomainModifier(recordType, record));
-      if (modifiers.length) {
-        D_EXTEND(name === "@" ? domain : `${name}.${domain}`, modifiers);
+      if (records?.length) {
+        D_EXTEND(
+          name === "@" ? domain : `${name}.${domain}`,
+          records.map((record) => recordToDomainModifier(recordType, record)),
+        );
       }
     });
   });
