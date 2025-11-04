62b1a8d0286258767525ee08774bbb45dbe2fa55
kasefuchs <kasefuchs@protonmail.com>
2025-11-04 22:08:34 +0300

    :construction_worker: Add deploy workflow

--- zones/rslib.config.ts
+++ zones/rslib.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig, type Format, type LibConfig, type RslibConfig } from "@rslib/core";
+import { defineConfig } from "@rslib/core";
 import { pluginYaml } from "@rsbuild/plugin-yaml";
 
-const formats: Format[] = ["esm", "cjs"];
-const libConfig: Partial<LibConfig> = {
-  dts: true,
-  syntax: "es5",
-};
-
 // noinspection JSUnusedGlobalSymbols
-export default defineConfig(
-  (): RslibConfig => ({
-    lib: formats.map(
-      (format, index): LibConfig => ({
-        ...libConfig,
-        format,
-        dts: libConfig?.dts && !index,
-      }),
-    ),
-    plugins: [pluginYaml()],
-  }),
-);
+export default defineConfig({
+  lib: [
+    {
+      format: "cjs",
+      syntax: "es5",
+      dts: true,
+    },
+  ],
+  plugins: [pluginYaml()],
+});
