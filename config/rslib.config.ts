a95bfe6f59717d21d6f464e9e0cc006d6229a082
kasefuchs <kasefuchs@protonmail.com>
2025-11-04 09:06:31 +0300

    :construction: Initialize project structure

---
+++ config/rslib.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from "@rslib/core";
+
+// noinspection JSUnusedGlobalSymbols
+export default defineConfig({
+  lib: [
+    {
+      format: "umd",
+      syntax: "es5",
+      banner: {
+        // language=JavaScript
+        js: "var global = this;"
+      },
+    },
+  ],
+});
